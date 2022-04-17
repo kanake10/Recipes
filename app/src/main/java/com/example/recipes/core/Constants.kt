cbff4dca3f7f36d4af5e082c836569bf04a658b3
kanake10 <10kanake@gmail.com>
2022-04-17 23:11:08 +0300

    utils

---
+++ app/src/main/java/com/example/recipes/core/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.recipes.core
+
+object Constants {
+    const val BASE_URL = "https://hf-android-app.s3-eu-west-1.amazonaws.com/android-test/"
+    const val END_POINTS = "recipes.json"
+}
